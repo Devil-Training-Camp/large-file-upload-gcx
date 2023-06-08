7c46c0d7574c9ca8bfa85bc178f95e30da66d94e
gchaoxu <xgc0829@163.com>
2023-06-08 23:48:47 +0800

    通过 web worker 使用 spark-md5 计算 hash 值
    feat: 使用 spark-md5 计算 hash 值
    
    feat: 使用 web worker 计算 hash 值

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import { useCallback, useState } from "react";
+import { useCallback, useState, useRef } from "react";
 import { createFileChunk } from "./util";
 import { request } from "./util/http";
 import { BASE_URL, SIZE } from "./constants";
@@ -7,7 +7,11 @@ import "./App.css";
 function App() {
   const [originFile, setOriginFile] = useState(undefined);
   const [fileChunkList, setFileChunkList] = useState([]);
-  const [totalProgress, setTotalProgress] = useState(0);
+  // 上传文件至服务器的进度
+  const [fileProgress, setFileProgress] = useState(0);
+
+  const workerRef = useRef(new Worker(new URL("./util/worker.js", import.meta.url)));
+  const worker = workerRef.current;
 
   const handleFileChange = async (e) => {
     const [file] = e.target.files;
@@ -24,11 +28,9 @@ function App() {
 
         // 设置总上传进度
         const totalPercentage = newList.reduce((total, chunk) => total + chunk.percentage, 0) / newList.length;
-        setTotalProgress(totalPercentage);
+        setFileProgress(totalPercentage);
         return newList;
       });
-
-      console.log(totalProgress, fileChunkList);
     },
     [fileChunkList]
   );
@@ -68,10 +70,33 @@ function App() {
       }),
     });
   };
+
+  // 生成文件 hash（web-worker）
+  const calculateHash = (fileChunkList) => {
+    return new Promise((resolve) => {
+      let result = { fileHash: "", chunkHash: [], percentage: "" };
+      // 添加 worker 属性
+      worker.postMessage({ fileChunkList });
+      worker.onmessage = (e) => {
+        const { percentage, fileHash, chunkHash } = e.data;
+        result.percentage = percentage;
+        if (fileHash) {
+          result.fileHash = fileHash;
+          resolve(result);
+        } else {
+          result.chunkHash.push(chunkHash);
+        }
+      };
+    });
+  };
+
   const handleUpload = async () => {
     if (!originFile) return alert("请上传文件");
     const fileChunk = createFileChunk(originFile);
+    const allChunkFilesHash = await calculateHash(fileChunk); // 计算所有切片的hash
+    // console.log(allChunkFilesHash);
     const fileChunkList = fileChunk.map(({ file }, index) => ({
+      fileHash: allChunkFilesHash[index],
       chunk: file,
       index,
       // 文件名 + 数组下标
